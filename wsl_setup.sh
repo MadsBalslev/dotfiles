f43cc6be6d059eb2391a097f3ff0a2afd4ea3e0b
Mads Balslev <madspbalslev@gmail.com>
2021-11-21 15:45:58 +0100

    Update wsl_setup.sh

--- wsl_setup.sh
+++ wsl_setup.sh
@@ -1,3 +1,28 @@
-#Node.js LTS
-curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# Install ZSH
+sudo apt install zsh -y
+
+# Change shell to ZSH
+chsh -s $(which zsh) 
+touch ~/.zshrc
+
+#Install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+########################
+#       Git Setup     #
+#######################
+# Install latest version of git
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt update
+sudo apt install git -y
+git config --global user.name "Mads Balslev"
+git config --global user.email madspbalslev@gmail.com
+git config --global init.defaultBranch main
+git config --global color.ui auto
+
+git clone https://github.com/MadsBalslev/dotfiles.git ~/.dotfiles
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+mv ~/.dotfiles/mads.zsh-theme ~/.oh-my-zsh/themes
+mv ~/.dotfiles/.zshrc_wsl ~/.zshrc
